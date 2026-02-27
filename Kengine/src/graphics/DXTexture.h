e00289c30ddde9a72f635d01c3d0476f0b5ec8ff
Adam Williams <2willa59@solent.ac.uk>
2026-02-27 17:11:22 +0000

    filtering

--- Kengine/src/graphics/DXTexture.h
+++ Kengine/src/graphics/DXTexture.h
@@ -1,5 +1,25 @@
 #pragma once
+#include <string>
+
+class DXRenderer;
+
+struct ID3D11ShaderResourceView;
+struct ID3D11SamplerState;
+
 class DXTexture
 {
+
+public:
+
+	DXTexture(DXRenderer& dxRend, std::string assetPath);
+	~DXTexture();
+
+	//TODO: transparency and clipping textures
+
+private:
+
+	ID3D11SamplerState* sampler = nullptr;
+	ID3D11ShaderResourceView* texture = nullptr;
+
 };
 
