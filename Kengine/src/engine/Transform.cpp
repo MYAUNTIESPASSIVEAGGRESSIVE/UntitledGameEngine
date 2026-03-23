f2efaa582093d5eb55bc0ce1f3110414755c5b6d
MYAUNTIESPASSIVEAGRESSIVE <adamkw123lightning@gmail.com>
2026-03-23 14:25:43 +0000

    Matrix

--- Kengine/src/engine/Transform.cpp
+++ Kengine/src/engine/Transform.cpp
@@ -1,29 +1,61 @@
 #include "Transform.h"
 namespace Kengine
 {
+	Matrix Transform::GetWorldMatrix()
+	{
+		Matrix scaleMat = ScaleFromVector(scale);
+		Matrix rotationMat = RotationFromVector(rotation);
+		Matrix translationMat = TranslateFromVector(position);
+
+		return scaleMat * rotationMat * translationMat;
+	}
+
 	void Transform::Translate(Vector3 translation)
 	{
 		position = operator+(position, translation);
 	}
 
-	void Transform::Rotate(Vector3 inRotation)
+	void Transform::Rotate(float inRotation)
 	{
-
+		RotateVector3(rotation, inRotation);
 	}
 
 	Vector3 Transform::GetForward()
 	{
-		return Vector3();
+		float pitch = rotation.x;
+		float yaw = rotation.y;
+
+		Vector3 direction
+		{
+			cosf(pitch) * sinf(yaw),
+			sinf(pitch),
+			cosf(pitch) * cosf(yaw),
+		};
+
+		return direction.Normalized();
 	}
 
 	Vector3 Transform::GetRight()
 	{
-		return Vector3();
+		float pitch = rotation.x;
+		float yaw = rotation.y;
+		float roll = rotation.z;
+
+		Vector3 direction
+		{
+			cosf(roll) * cosf(yaw) + sinf(roll) * sinf(pitch) * sinf(yaw),  // X
+			sinf(roll) * cosf(pitch),									    // Y
+			cosf(roll) * -sinf(yaw) + sinf(roll) * sinf(pitch) * cosf(yaw), // Z
+		};
+
+		return direction.Normalized();
 	}
 
 	Vector3 Transform::GetUp()
 	{
-		return Vector3();
+		Vector3 cross = CrossProduct(GetForward(), GetRight());
+
+		return cross.Normalized();
 	}
 }
 
